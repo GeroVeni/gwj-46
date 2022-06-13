eabedcd4d352f097246b454d20829a0fb21dcb19
georgebaint <georgebaint@hotmail.com>
2022-06-14 01:15:40 +0300

    Added maze, walls, portal, spikes

---
+++ scripts/portal.gd
@@ -0,0 +1,52 @@
+extends Area2D
+
+@onready var portal_timer: Timer = $PortalTimer
+@export var activation_time := 0.5
+@export var entry_color = Color(0, 0.75, 1)
+@export var exit_color = Color(1, 0.5, 0)
+@export var activation_color = Color(0.5, 0.7, 0.9)
+@export var is_entry = 0
+@export_node_path(Area2D) var exit_portal_path
+var exit_portal : Area2D
+var trap_active = 0
+var is_active = 0
+var player_body : Node2D
+func _process(delta):
+	if player_body:
+		if portal_timer.is_stopped():
+			if player_body.velocity == Vector2() and is_active:
+				portal_timer.start(activation_time)
+				print("test")
+			else:
+				portal_timer.stop()
+			
+		
+	
+func _ready():
+	if exit_portal_path:
+		exit_portal = get_node(exit_portal_path)
+	if is_entry:
+		$MeshInstance2D.modulate = entry_color
+	else:
+		$MeshInstance2D.modulate = exit_color
+		
+
+func _on_portal_body_entered(body):
+	if body.is_in_group("player"):
+		player_body = body
+		if is_entry:
+			is_active = 1
+			$MeshInstance2D.modulate = activation_color
+				
+			
+
+
+func _on_portal_timer_timeout():
+	player_body.global_position = exit_portal.global_position
+	portal_timer.stop()
+	player_body = null
+	
+
+
+func _on_portal_body_exited(body):
+	is_active = 0
---
+++ scripts/spikes.gd
@@ -0,0 +1,37 @@
+extends Area2D
+
+@onready var trap_timer: Timer = $TrapTimer
+@export var activation_time := 0.5
+@export var trap_cooldown_time := 3.0
+@export var default_color = Color(0, 0, 0)
+@export var warning_color = Color(1, 0.6, 0)
+@export var activation_color = Color(1, 0, 0)
+@export_enum(Enter, Exit) var portalType
+var trap_active = 0
+
+
+func _on_spikes_body_entered(body):
+	if !trap_active:
+		trap_timer.start(activation_time)
+		$MeshInstance2D.modulate = warning_color
+		trap_active = 1
+		
+		if !portalType:
+			print("it works")
+	
+	
+
+func _on_trap_timer_timeout():
+	if trap_active:
+		#trap_timer.stop()
+		$MeshInstance2D.modulate = activation_color
+		trap_timer.start(trap_cooldown_time)
+		#trap_timer.stop()
+		trap_active = 0
+	else:
+		$MeshInstance2D.modulate = default_color 
+		trap_timer.stop()
+		
+	
+	
+	
